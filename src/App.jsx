a487e6fd8afc51a0615c32d3e91674eaa405ce40
Laranaya <141862629+Laranaya@users.noreply.github.com>
2025-11-09 15:19:49 +0000

    First commit for Cineverse

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+     <div>
+      <h1 className="headerOne">Welcome to Cineverse!</h1>
+        <p className="ultimatePara">The Ultimate Sci-Fi Movie Recommendation System</p>
+      </div>
+    </>
+  )
+}
+
+export default App
